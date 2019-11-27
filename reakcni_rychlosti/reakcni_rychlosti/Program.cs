c83545ce557d8ce756e4c7bedadfc64a76bf81f2
Jan Sáblík <jan.sablik00@gmail.com>
2019-11-27 16:38:11 +0100

    Vytvoreni projektu

---
+++ reakcni_rychlosti/reakcni_rychlosti/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reakcni_rychlosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
